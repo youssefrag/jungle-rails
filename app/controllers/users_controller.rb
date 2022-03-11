ff307ec8273b65c8f1e177fe4193f56499f1d495
Youssef Ragab <ragab.youssef95@gmail.com>
2022-03-11 13:48:18 -0500

    add login and register pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,16 @@
 class UsersController < ApplicationController
   def new
-    @user=User.new
+    @user = User.new
   end
 
   def create
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = @user.id
       redirect_to root_path
     else
-      render :new
+      redirect_to '/signup'
     end
   end
 
@@ -18,7 +19,8 @@ class UsersController < ApplicationController
       :first_name,
       :last_name,
       :email,
-      :password
+      :password_digest,
+      :password_confirmation
     )
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -24,8 +24,11 @@ Rails.application.routes.draw do
   get 'about', to: 'about#index'
 
   get 'signup', to: 'users#new'
-
   post 'users', to: 'users#create'
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  get 'logout' => 'sessions#destroy'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
